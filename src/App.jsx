a6a595e8eec8740b5631007089dbfb9701cf8e85
Daniel Alvarez <dan.alvarez7@gmail.com>
2026-01-01 13:11:17 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ArticleCard from "./components/ArticleCard";
 import "./App.css";
+import Footer from "./components/Footer";
 
 export default function App() {
   const [articles, setArticles] = useState([]);
@@ -46,82 +47,86 @@ export default function App() {
   };
 
   return (
-    <div className="mx-auto max-w-7xl px-6 py-10">
-      {/* Header */}
-      <header className="mb-8 max-w-3xl">
-        <h1 className="flex items-baseline gap-2 text-3xl font-bold text-slate-900">
-          <span>Scilents AI</span><span className="hidden sm:flex sm:leading-none text-sm">Your AI Research Assistant</span>
-        </h1>
-        <span className="block sm:hidden font-bold text-sm">Your AI Research Assistant</span>
-        <p className="mt-2 text-slate-600">
-          
-        </p>
-        <p className="mt-2 text-slate-600">
-          Instant key concepts from scientific literature
-        </p>
+    <div className='min-h-screen flex flex-col'>
 
-      </header>
+      <div className="flex-grow max-w-7xl px-6 py-10">
+        {/* Header */}
+        <header className="mb-8 max-w-3xl">
+          <h1 className="flex items-baseline gap-2 text-3xl font-bold text-slate-900">
+            <span>Scilents AI</span><span className="hidden sm:flex sm:leading-none text-sm">Your AI Research Assistant</span>
+          </h1>
+          <span className="block sm:hidden font-bold text-sm">Your AI Research Assistant</span>
+          <p className="mt-2 text-slate-600">
+            
+          </p>
+          <p className="mt-2 text-slate-600">
+            Instant key concepts from scientific literature
+          </p>
 
-      {/* Search bar */}
-      <form
-        onSubmit={handleSubmit}
-        className="mb-10 flex gap-2"
-      >
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search by topic or research area."
-          className="flex-1 rounded-xl border border-slate-300 px-4 py-2.5 text-sm
-                    focus:border-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-200"
-        />
+        </header>
 
-        <button
-          type="submit"
-          disabled={loading}
-          className="rounded-xl bg-teal-600 px-5 py-2.5 text-sm font-semibold text-white
-                    transition hover:bg-teal-700 disabled:opacity-50"
+        {/* Search bar */}
+        <form
+          onSubmit={handleSubmit}
+          className="mb-10 flex gap-2"
         >
-          Search
-        </button>
-      </form>
-
-      {/* Status */}
-      {loading && (
-        <p className="text-sm text-slate-500">Searching articles…</p>
-      )}
-
-      {error && (
-        <div className="my-2">
-            <p className="text-sm text-red-600">{error}</p>
-            <p className="text-sm mt-2">Since this is still ssan experimental app, the API token might have reach the rate limit, please wait a few seconds and try again</p>
-        </div>
-      )}
-
-      {!loading && articles.length === 0 && searched && !error && (
-        <p className="text-sm text-slate-500">
-          No results found for “{query}”
-        </p>
-      )}
-
-      {!loading && articles.length > 0 && lastQuery && !error && (
-        <p className="mb-6 text-sm text-slate-600">
-          Results for query:{" "}
-          <span className="font-medium text-teal-700">
-            “{lastQuery}”
-          </span>
-        </p>
-)}
-
-      {/* Articles */}
-      <div className="mt-6 space-y-6">
-        {articles.map((article, index) => (
-          <ArticleCard
-            key={article.pmid || article.doi || index}
-            article={article}
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search by topic or research area."
+            className="flex-1 rounded-xl border border-slate-300 px-4 py-2.5 text-sm
+                      focus:border-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-200"
           />
-        ))}
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="rounded-xl bg-teal-600 px-5 py-2.5 text-sm font-semibold text-white
+                      transition hover:bg-teal-700 disabled:opacity-50"
+          >
+            Search
+          </button>
+        </form>
+
+        {/* Status */}
+        {loading && (
+          <p className="text-sm text-slate-500">Searching articles…</p>
+        )}
+
+        {error && (
+          <div className="my-2">
+              <p className="text-sm text-red-600">{error}</p>
+              <p className="text-sm mt-2">Since this is still ssan experimental app, the API token might have reach the rate limit, please wait a few seconds and try again</p>
+          </div>
+        )}
+
+        {!loading && articles.length === 0 && searched && !error && (
+          <p className="text-sm text-slate-500">
+            No results found for “{query}”
+          </p>
+        )}
+
+        {!loading && articles.length > 0 && lastQuery && !error && (
+          <p className="mb-6 text-sm text-slate-600">
+            Results for query:{" "}
+            <span className="font-medium text-teal-700">
+              “{lastQuery}”
+            </span>
+          </p>
+  )}
+
+        {/* Articles */}
+        <div className="mt-6 space-y-6">
+          {articles.map((article, index) => (
+            <ArticleCard
+              key={article.pmid || article.doi || index}
+              article={article}
+            />
+          ))}
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+
+    <footer className="footer sm:footer-horizontal footer-center text-base-content p-4">
+      <p className="text-center text-sm text-slate-600">Copyright © {new Date().getFullYear()} scilents-ai.</p>
+    </footer>
+  )
+}
+
+export default Footer
